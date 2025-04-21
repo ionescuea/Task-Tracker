29c9b11d1cc725b57b239dacc82fcff8ab1f2cec
ionescuea <60351947+ionescuea@users.noreply.github.com>
2025-04-21 21:48:53 +0100

    Create delete task and update task routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,19 +1,10 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
-const tasks = [
-  {
-    title: "First task",
-    description: "Create first task in the current project",
-    status: "completed"
-  },
-  {
-    title: "Second task",
-    description: "Create a second task in the current project",
-    status: "pending"
-  }
-];
+let tasks = [];
 
 router.get('/', (req, res) => {
   console.log(tasks);
@@ -22,9 +13,32 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const newTask = req.body;
-  tasks.push(newTask);
+  tasks.push({ ...newTask, id: uuidv4() });
   res.send(`Task "${newTask.title}" added successfully!`);
-  // res.status(201).json(newTask);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = (req.params);
+
+  const foundTask = tasks.find((task) => task.id === id);
+  res.send(foundTask);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = (req.params);
+  tasks = tasks.filter((task) => task.id !== id);
+  res.send(`Task with id ${id} deleted successfully!`);
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = (req.params);
+  const { status } = req.body;
+
+  const statusUpdate = tasks.find((task) => task.id === id);
+  if (status) {
+    statusUpdate.status = status;
+    res.send(`Task with id ${id} updated successfully!`);
+  }
 });
 
 export default router;
