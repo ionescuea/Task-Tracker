bacbc52dff47f16755f5dbd3f603ba810b1f3f6b
ionescuea <60351947+ionescuea@users.noreply.github.com>
2025-04-27 18:25:41 +0100

    Update packages

--- server/db.js
+++ server/db.js
@@ -1,17 +1,15 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+// Load environment variables from .env file
 dotenv.config();
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-    console.log('MongoDB Atlas connected!');
-  } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+    await mongoose.connect(process.env.MONGO_URI);  
+    console.log("MongoDB Atlas connected!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
   }
 };
 
